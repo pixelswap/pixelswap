b2e02b8c2bf7944898f8eba71fb2682cff69177d
len.liu <chunlongliu@trip.com>
2021-02-18 20:17:11 +0800

    增加sys-info

--- src/components/PixelContent.vue
+++ src/components/PixelContent.vue
@@ -1,12 +1,19 @@
 <template>
   <main class="main-content">
-    <!--
-    <a href="https://github.com/nostalgic-css/NES.css" target="_blank" rel="noopener" class="github-link"
-       v-bind:class="{active: isActive}">
-      <p class="nes-balloon from-right">Go, Play Game!~</p>
-      <i class="nes-octocat animate"></i>
-    </a>
-    -->
+    <div class="sys-info">
+      <img src="../../public/logo_gray.svg">
+      <div class="sys-name">
+        <div class="line-circle">
+          <span class="left"></span>
+          <i class="circle"></i>
+        </div>
+        <p>链上像素艺术创作平台</p>
+        <div class="line-circle">
+          <i class="circle"></i>
+          <span class="right"></span>
+        </div>
+      </div>
+    </div>
     <router-view></router-view>
   </main>
 </template>
@@ -18,3 +25,57 @@
     ]
   }
 </script>
+<style>
+  .sys-info {
+    display: block;
+    position: fixed;
+    width: 100%;
+    height: auto;
+    text-align: center;
+    margin-top: 10px;
+    z-index: -1;
+  }
+
+  .sys-name {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .line-circle span.left {
+    margin-right: 2px;
+    background: linear-gradient(90deg,rgba(188,191,208,0) 0,#bcbfd0);
+  }
+
+  .line-circle span.right {
+    margin-left: 2px;
+    background: linear-gradient(270deg,rgba(188,191,208,0) 0,#bcbfd0);
+  }
+
+  .line-circle span {
+    width: 16px;
+    height: 1px;
+    display: inline-block;
+    vertical-align: middle;
+  }
+
+  .line-circle .circle {
+    margin-bottom: 0;
+    width: 2px;
+    height: 2px;
+    background: #6e7a87;
+    border-radius: 2px;
+  }
+
+  .line-circle .circle, .line-circle span {
+    display: inline-block;
+    vertical-align: middle;
+  }
+
+  .sys-info p {
+    font-family: "Noto Sans", sans-serif;
+    letter-spacing: -0.018em;
+    color: #6e7a87;
+    margin: 5px 5px;
+  }
+</style>
